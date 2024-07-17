664f2a6ba3b589cd9008990986df43fba32431ea
shirley-dw <vokacshirley@gmail.com>
2024-07-17 19:00:01 -0300

    Modificaciones en el App

--- Mensajeria/src/Chat/ChatHeaderInfo/ChatHeaderInfo.jsx
+++ Mensajeria/src/Chat/ChatHeaderInfo/ChatHeaderInfo.jsx
@@ -1,8 +1,7 @@
-
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Link } from 'react-router-dom'
-import './ChatHeaderInfo.css'
+import { Link } from "react-router-dom";
+import './ChatHeaderInfo.css';
 /* Import imagenes */
 /* import {Imagenes} from '../../../public/Imagenes'; */
 /* Importo contactos */
@@ -12,38 +11,54 @@ import { SlArrowLeft } from "react-icons/sl";
 import { GoDeviceCameraVideo } from "react-icons/go";
 import { MdPhone } from "react-icons/md";
 import { IoMenu } from "react-icons/io5";
-import { Imagenes } from '../../../public/Imagenes';
 
 /*  Define un componente llamado ChatHeaderInfo y lo exporta para ser usado en otros archivos.
  El componente devuelve un elemento div con la clase chat-header-info,(Contiene el encabezado de la pantalla de chat */
+
 const ChatHeaderInfo = () => {
-  const { contactoID } = useParams();
+  const { id } = useParams();
   const [contactos, setContactos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     ObtenerContactos()
       .then((contactos) => {
         setContactos(contactos);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("Error al obtener contactos:", error);
+        setLoading(false);
       });
   }, []);
 
-  const contacto = contactos.find((contacto) => contacto.id === Number(contactoID));
-  const imgSrc = Imagenes[contacto.thumbnail];
+  const dataContacto = contactos.find((contacto) => contacto.id === Number(id));
+
+  if (loading) {
+   console.log('Cargando')
+  }
+  else
+  {
 
   return (
     <div className="chat-header-info">
-      <div className="contacto"><Link to="/contactos"><SlArrowLeft className="arrow" /></Link>
-        <img src={imgSrc} />
+      <div className="contacto">
+        <Link to="/">
+          <SlArrowLeft className="arrow" />
+        </Link>
+        <img className="fotoperfil" src={'/Imagenes/'+ dataContacto.thumbnail} alt="Foto perfil" />
         <div className="chat-header">
-          <div className="profile-name">{contacto.nombre}</div>
-          <div className="status-text">Online</div>
-        </div></div>
+          <div className="profile-name">{dataContacto.nombre}</div>
+          <div className="status-text">{dataContacto.ultima_conexion}</div>
+        </div>
+      </div>
       <div className="icons">
         <GoDeviceCameraVideo />
-        <MdPhone /><IoMenu />
+        <MdPhone />
+        <IoMenu />
       </div>
     </div>
-  )
+  );
+};
 }
-
-export default ChatHeaderInfo
+export default ChatHeaderInfo;
